d00d1417a3a2fed07b48ee2cfe0cad5ff6234536
Gabriel Ricaldone <gabriel.ricaldone@jcmaxwell.it>
2023-04-10 22:15:46 +0200

    Avanti e indietro menu

--- src/proggestionale/ProgGestionale.java
+++ src/proggestionale/ProgGestionale.java
@@ -9,6 +9,7 @@ import java.awt.Color;
 import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.ArrayList;
 import javax.swing.Box;
 import javax.swing.JButton;
 import javax.swing.JFrame;
@@ -25,73 +26,117 @@ import static javax.swing.WindowConstants.EXIT_ON_CLOSE;
 public class ProgGestionale {
 
     JFrame frame = new JFrame("soos");
-    JTextField nominativo;
-    JTextField numeroTel;
+    
+    JPanel pannelloProg;
     JPanel pannelloPopUp;
     JPanel pannelloMenu;
+    JPanel pannelloRubrica;
+    JPanel pannelloRimuovi;
+    
+    JTextField nominativo;
+    JTextField numeroTel;
+    
     JLabel titolo;
+    
     JButton pulsanteAggiunta;
     JButton pulsanteRimuovi;
     JButton pulsanteVisione;
-
+    JButton pulsanteIndietroRimuovi;
+    JButton pulsanteIndietroContatti;
+    ActionListener indietro;
+    ArrayList<Contatto> rubrica;
+    
     ProgGestionale() {
+        rubrica = new ArrayList<Contatto>();
+        
+        pannelloProg = new JPanel();
+        pannelloRubrica = new JPanel();
+        pannelloMenu = new JPanel();
+        pannelloRimuovi = new JPanel();
+        
+        titolo = new JLabel("Rubrica");
+        
+        pulsanteAggiunta = new JButton("Aggiungi contatto");
+        pulsanteRimuovi = new JButton("Rimuovi contatto");
+        pulsanteVisione = new JButton("Vedi contatti");
+        pulsanteIndietroContatti = new JButton("Indietro");
+        pulsanteIndietroRimuovi = new JButton("Indietro");
+        
         pannelloPopUp = new JPanel();
         numeroTel = new JTextField(10);
         nominativo = new JTextField(30);
-
+        
         pannelloPopUp.add(new JLabel("Nominativo:"));
         pannelloPopUp.add(nominativo);
-        pannelloPopUp.add(Box.createVerticalStrut(15)); // a spacer
         pannelloPopUp.add(new JLabel("Telefono:"));
         pannelloPopUp.add(numeroTel);
-
-        pannelloMenu = new JPanel();
-        titolo = new JLabel("Rubrica");
-
+        
         frame.setLayout(null);
-        frame.add(pannelloMenu);
         frame.setBounds(0, 0, 360, 500);
         frame.setDefaultCloseOperation(EXIT_ON_CLOSE);
 
+        pannelloRimuovi.setBounds(0, 0, frame.getWidth(), frame.getHeight());
+        pannelloRimuovi.setBackground(Color.blue);
+        pannelloRimuovi.setVisible(false);
+        pannelloRimuovi.add(pulsanteIndietroRimuovi);
+                
+        pannelloRubrica.setBounds(0, 0, frame.getWidth(), frame.getHeight());
+        pannelloRubrica.setBackground(Color.red);
+        pannelloRubrica.setVisible(false);
+        pannelloRubrica.add(pulsanteIndietroContatti);
+        
         pannelloMenu.setBounds(0, 0, frame.getWidth(), frame.getHeight());
         pannelloMenu.setLayout(new GridLayout(6, 0));
 
-        pulsanteAggiunta = new JButton("Aggiungi contatto");
+        pannelloMenu.add(titolo);
+        pannelloMenu.add(pulsanteAggiunta);
+        pannelloMenu.add(pulsanteRimuovi);
+        pannelloMenu.add(pulsanteVisione);
+        
+        frame.add(pannelloRubrica);
+        frame.add(pannelloMenu);
+        frame.add(pannelloRimuovi);
+        
+        indietro = new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                
+                pannelloMenu.setVisible(true);
+                pannelloRubrica.setVisible(false);
+                pannelloRimuovi.setVisible(false);
+            }
+        };
+        
+        
+        pulsanteIndietroContatti.addActionListener(indietro);
+        pulsanteIndietroRimuovi.addActionListener(indietro);
+        
         pulsanteAggiunta.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
                 int result = JOptionPane.showConfirmDialog(null, pannelloPopUp,
                         "Aggiungi un contatto", JOptionPane.OK_CANCEL_OPTION);
                 if (result == JOptionPane.OK_OPTION) {
-                    System.out.println("x value: " + nominativo.getText());
-                    System.out.println("y value: " + numeroTel.getText());
+                    rubrica.add(new Contatto(nominativo.getText(),numeroTel.getText()));
                 }
             }
-
         });
 
-        pulsanteRimuovi = new JButton("Rimuovi contatto");
         pulsanteRimuovi.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-
+                pannelloMenu.setVisible(false);
+                pannelloRimuovi.setVisible(true);
             }
-
         });
-
-        pulsanteVisione = new JButton("Vedi contatti");
+        
         pulsanteVisione.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-
+                pannelloMenu.setVisible(false);
+                pannelloRubrica.setVisible(true);
             }
-
         });
-
-        pannelloMenu.add(titolo);
-        pannelloMenu.add(pulsanteAggiunta);
-        pannelloMenu.add(pulsanteRimuovi);
-        pannelloMenu.add(pulsanteVisione);
 
         frame.setVisible(true);
     }
